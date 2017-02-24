67341d20552388a7dc6882dcd33a32bcd587bc27
michellekondou <michellekondou@gmail.com>
2017-02-24 14:33:01 +0200

    add fb open graph

--- wp-content/themes/vivlio/templates/head.php
+++ wp-content/themes/vivlio/templates/head.php
@@ -3,4 +3,10 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <meta property="fb:app_id"          content="" /> 
+  <meta property="og:type"            content="article" /> 
+  <meta property="og:url"             content="//vivl.io" /> 
+  <meta property="og:title"           content="ασδασ" /> 
+  <meta property="og:image"           content="https://michellekondou.me/vivlio/wp-content/uploads/08.jpg" /> 
+  <meta property="og:description"    content="ασδασδ" />
 </head>
