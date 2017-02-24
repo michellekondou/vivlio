33e083190081cc23d77979d56a7a0c45cfb34100
michellekondou <michellekondou@gmail.com>
2017-02-24 15:11:59 +0200

    fb og corrections

--- wp-content/themes/vivlio/templates/head.php
+++ wp-content/themes/vivlio/templates/head.php
@@ -3,9 +3,12 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <meta property="og:type"            content="article" /> 
-  <meta property="og:url"             content="https://vivl.io" /> 
-  <meta property="og:title"           content="Vivlio" /> 
-  <meta property="og:image"           content="https://vivl.io/wp-content/uploads/universe-landscape.jpg" /> 
-  <meta property="og:description"    content="We’re a publisher of classics, aiming to revitalise the experience  of reading great books." />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="Vivlio" />
+  <meta property="og:description" content="The Universe of Tom Sawyer A print & digital publishing experiment." />
+  <meta property="og:url" content="https://vivl.io/" />
+  <meta property="og:site_name" content="vivlio" />
+  <meta property="og:image" content="https://vivl.io/wp-content/uploads/universe-landscape.jpg" />
+  <meta property="og:locale" content="en_US" />
+  <meta name="twitter:card" content="summary" />
 </head>
