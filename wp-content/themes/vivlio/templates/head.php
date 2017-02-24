8316b621f0febfad3169d57fe81499b7a3bc246c
michellekondou <michellekondou@gmail.com>
2017-02-24 15:00:20 +0200

    fb og corrections

--- wp-content/themes/vivlio/templates/head.php
+++ wp-content/themes/vivlio/templates/head.php
@@ -3,10 +3,9 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <meta property="fb:app_id"          content="1734250263554943" /> 
   <meta property="og:type"            content="website" /> 
-  <meta property="og:url"             content="//vivl.io" /> 
+  <meta property="og:url"             content="https://vivl.io" /> 
   <meta property="og:title"           content="Vivlio" /> 
-  <meta property="og:image"           content="//vivl.io/wp-content/uploads/universe-landscape.jpg" /> 
+  <meta property="og:image"           content="https://vivl.io/wp-content/uploads/universe-landscape.jpg" /> 
   <meta property="og:description"    content="We’re a publisher of classics, aiming to revitalise the experience  of reading great books." />
 </head>
